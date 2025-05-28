cb849c14bcf71db60a4e489a877af8bdeaf617ca
NikVince <165183152+NikVince@users.noreply.github.com>
2025-05-28 23:06:20 +0200

    UPDATED problem '12' with solution that is passing test.

--- problems/12-array-converter.js
+++ problems/12-array-converter.js
@@ -13,6 +13,17 @@ console.log(arrayConverter(["apple", "banana", "potato", "banana"])); // => {app
 
 function arrayConverter(array) {
   // Your code here
+  let countObj = {};
+
+  for (let elem of array) {
+    if (countObj[elem] === undefined) {
+      countObj[elem] = 1;
+    } else {
+      countObj[elem]++;
+    }
+  }
+
+  return countObj;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
