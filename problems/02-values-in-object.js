8f11057c91344b277aa11be551f397a016edb548
NikVince <165183152+NikVince@users.noreply.github.com>
2025-05-28 22:20:29 +0200

    UPDATED problem '02' with solutions that are passing tests.

--- problems/02-values-in-object.js
+++ problems/02-values-in-object.js
@@ -1,6 +1,6 @@
 /***********************************************************************
-Write a function called `valuesInObject(obj)` that takes in an object and returns 
-an array of all the values within that Object. 
+Write a function called `valuesInObject(obj)` that takes in an object and returns
+an array of all the values within that Object.
 
 
 Do this once using using a `for...in` loop and once using `Object.values`.
@@ -16,6 +16,15 @@ valuesInObject(foods); // => ["tart", "sour", "sweet"]
 
 function valuesInObject(obj) {
   // Your code here
+  // return Object.values(obj); // Solution 1
+
+  let arr = [];
+
+  for (keys in obj) {
+    arr.push(obj[keys]);
+  }
+
+  return arr;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
