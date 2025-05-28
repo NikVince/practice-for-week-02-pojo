f7214d68ca9d2bb90cc8e0159eaa02c707fbba80
NikVince <165183152+NikVince@users.noreply.github.com>
2025-05-28 23:15:22 +0200

    UPDATED problem '14' with solution that is passing test.

--- problems/14-count-scores.js
+++ problems/14-count-scores.js
@@ -29,6 +29,20 @@ console.log(countScores(peeps)); //=> { Anthony: 4, Fred: 4, Winnie: 6 }
 
 function countScores(people) {
   // Your code here
+    let countObj = {};
+
+    for (let person of people) {
+      let name = person.name;
+      let score = person.score;
+
+      if (countObj[name] === undefined) {
+        countObj[name] = score;
+      } else {
+        countObj[name] += score;
+      }
+    }
+
+  return countObj;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
